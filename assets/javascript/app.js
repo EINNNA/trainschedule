1f26aa88d8e1955c08a914bdf09707515ef8f58a
EINNNA <annie_t_@hotmail.com>
2019-09-14 20:11:20 +1000

    links added

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBQBhKPWiAoENvX0Z9nlYyYgMHt62oLkbk",
+    authDomain: "fir-bootcamp-f51e0.firebaseapp.com",
+    databaseURL: "https://fir-bootcamp-f51e0.firebaseio.com",
+    projectId: "fir-bootcamp-f51e0",
+    storageBucket: "fir-bootcamp-f51e0.appspot.com",
+    messagingSenderId: "239896199354",
+    appId: "1:239896199354:web:5d71f05cbf64871a014283"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  
+  var database = firebase.database();
